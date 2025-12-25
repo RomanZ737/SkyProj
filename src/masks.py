2bde3e842bacf6bb77fcbe59c878398f738b3859
Roman <pomanz@mail.ru>
2025-12-25 17:34:01 +0300

    initial commit

---
+++ src/masks.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+
+def get_mask_card_number(card_number: Union[str | int]) -> str:
+    """Принимает номер карты в виде числа или строки и возвращает маску номера по правилу
+    XXXX XX** **** XXXX"""
+    if type(card_number) is str:  # проверяем тип данных
+        card_num_mask = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:16]}"
+    else:
+        card_number_str = str(card_number)  # преобразуем числовую переменную в троку
+        card_num_mask = f"{card_number_str[:4]} {card_number_str[4:6]}** **** {card_number_str[12:16]}"
+
+    return card_num_mask
+
+
+def get_mask_account(account_number: Union[str | int]) -> str:
+    """Принимает номер счета в виде числа или строки и возвращает маску номера по правилу
+    **XXXX"""
+    if type(account_number) is str:  # проверяем тип данных
+        account_num_mask = f"**{account_number[-4:]}"
+    else:
+        account_number_str = str(account_number)  # преобразуем числовую переменную в троку
+        account_num_mask = f"**{account_number_str[-4:]}"
+
+    return account_num_mask
