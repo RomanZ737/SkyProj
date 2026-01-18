865c3a75a1f01860de5eb3bd944ec2fd63425a98
Roman <pomanz@mail.ru>
2026-01-18 14:46:01 +0300

    module mask updated

--- src/masks.py
+++ src/masks.py
@@ -4,7 +4,9 @@ from typing import Union
 def get_mask_card_number(card_number: Union[str | int]) -> str:
     """Принимает номер карты в виде числа или строки и возвращает маску номера по правилу
     XXXX XX** **** XXXX"""
-    if type(card_number) is str:  # проверяем тип данных
+    if not card_number:
+        return ""
+    elif type(card_number) is str:  # проверяем тип данных
         card_num_mask = f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:16]}"
     else:
         card_number_str = str(card_number)  # преобразуем числовую переменную в троку
@@ -16,10 +18,17 @@ def get_mask_card_number(card_number: Union[str | int]) -> str:
 def get_mask_account(account_number: Union[str | int]) -> str:
     """Принимает номер счета в виде числа или строки и возвращает маску номера по правилу
     **XXXX"""
-    if type(account_number) is str:  # проверяем тип данных
+    if not account_number:
+        return ""
+    elif type(account_number) is str:  # проверяем тип данных
         account_num_mask = f"**{account_number[-4:]}"
     else:
         account_number_str = str(account_number)  # преобразуем числовую переменную в троку
         account_num_mask = f"**{account_number_str[-4:]}"
 
     return account_num_mask
+
+
+if __name__ == "__main__":
+    print(get_mask_card_number("792289606361"))
+    print(get_mask_account(""))
