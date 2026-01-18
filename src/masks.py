300d8a0c1fa4de5f83b9f3ddb3423f265c36be33
Roman <pomanz@mail.ru>
2026-01-18 16:07:43 +0300

    Tempo

--- src/masks.py
+++ src/masks.py
@@ -27,8 +27,3 @@ def get_mask_account(account_number: Union[str | int]) -> str:
         account_num_mask = f"**{account_number_str[-4:]}"
 
     return account_num_mask
-
-
-if __name__ == "__main__":
-    print(get_mask_card_number("792289606361"))
-    print(get_mask_account(""))
--- src/widget.py
+++ src/widget.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 
 from src.masks import get_mask_account, get_mask_card_number
 
@@ -9,6 +9,8 @@ def mask_account_card(account_or_card_number: str) -> str:
     name_of_card_or_account = ""  # Название карты или счёт
     number_value_of_card_or_account = ""  # Номер карты или счёта
     card_or_account_mask = ""  # Маска номера карты или счёта
+    if type(account_or_card_number) is not str:
+        raise ValueError("Некорректные данные")
     for symbol in account_or_card_number:
         if symbol.isdigit():
             number_value_of_card_or_account += symbol
@@ -24,5 +26,8 @@ def mask_account_card(account_or_card_number: str) -> str:
 
 def get_date(data_iso_format: str) -> str:
     """Принимает дату в формате ISO-8601, возвращает дату в стандартном формате: ДД.ММ.ГГГГ"""
-    dt = datetime.strptime(data_iso_format, "%Y-%m-%dT%H:%M:%S.%f")
+    try:
+        dt = datetime.strptime(data_iso_format, "%Y-%m-%dT%H:%M:%S.%f")
+    except ValueError:
+        return "Неверный формат даты"
     return dt.strftime("%d.%m.%Y")
