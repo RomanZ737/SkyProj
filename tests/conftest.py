b661306a0ebdd7d191b7805b84a68ed757f47b0c
Roman <pomanz@mail.ru>
2026-01-18 19:56:51 +0300

    test for masks.py, processing.py and widget.py modules added

---
+++ tests/conftest.py
@@ -0,0 +1,107 @@
+import pytest
+
+
+@pytest.fixture
+def input_data_filter_by_state() -> list:
+    """Фикстура возвращает данные для тестирования функции filter_by_state"""
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+@pytest.fixture
+def output_data_filter_by_state_executed() -> list:
+    """Фукстура возвращает выходные данные данные filter_by_state с условием EXECUTED"""
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+@pytest.fixture
+def output_data_filter_by_state_canceled() -> list:
+    """Фукстура возвращает выходные данные данные filter_by_state с условием CANCELED"""
+    return [
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+@pytest.fixture
+def input_data_sort_by_date_1() -> list:
+    """Фукстура возвращает входные данные для функции sort_by_date"""
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+
+
+@pytest.fixture
+def input_data_sort_by_date_2() -> list:
+    """Фукстура возвращает входные данные для функции sort_by_date с одинаковой датой"""
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 594226727, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
+    ]
+
+
+@pytest.fixture
+def output_data_sort_by_date_1() -> list:
+    """Фукстура возвращает выходные данные для функции sort_by_date"""
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+@pytest.fixture
+def output_data_sort_by_date_2() -> list:
+    """Фукстура возвращает выходные данные для функции sort_by_date с одинаковой датой"""
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 594226727, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2019-07-03T18:35:29.512364"},
+    ]
+
+
+@pytest.fixture
+def output_data_sort_by_date_1_reversed() -> list:
+    """Фукстура возвращает выходные данные для функции sort_by_date с условием reverse=False"""
+    return [
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
+
+
+@pytest.fixture
+def input_data_sort_by_date_wrong_dates() -> list:
+    """Фукстура возвращает входные данные для функции sort_by_date с неверным форматом датам"""
+    return [
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-3002:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
+
+
+@pytest.fixture
+def output_data_sort_by_date_wrong_dates() -> list:
+    """Фукстура возвращает выходные данные для функции sort_by_date с неверным форматом датам"""
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-3002:08:58.425572"},
+    ]
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,27 +1,39 @@
-from src.widget import mask_account_card, get_date
 import pytest
 
-@pytest.mark.parametrize("account_or_card_number, expected_result", [
-    ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
-    ("Счет 73654108430135874305", "Счет **4305"),
-    ("Mastercard 7000792289606361", "Mastercard 7000 79** **** 6361"),
-    ("", "")
-])
-def test_mask_account_card(account_or_card_number, expected_result):
+from src.widget import get_date, mask_account_card
+
+
+@pytest.mark.parametrize(
+    "account_or_card_number, expected_result",
+    [
+        ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
+        ("Счет 73654108430135874305", "Счет **4305"),
+        ("Mastercard 7000792289606361", "Mastercard 7000 79** **** 6361"),
+        ("", ""),
+    ],
+)
+def test_mask_account_card(account_or_card_number: str, expected_result: str) -> None:
+    """Функция тестирует mask_account_card, стандартные разного вида данные на вход, формат str, и подача постой строки на вход"""
     assert mask_account_card(account_or_card_number) == expected_result
 
 
-def test_wrong_data_mask_account_card():
+def test_wrong_data_mask_account_card() -> None:
+    """Функция тестирует mask_account_card, на вход подаются данные формата int"""
     with pytest.raises(ValueError) as exc_info:
         mask_account_card(123547467)
 
     assert str(exc_info.value) == "Некорректные данные"
 
-@pytest.mark.parametrize('iso_date, expected_result', [
-    ('2024-03-11T02:26:18.671407', '11.03.2024'),
-    ('2025-04-11T02:26:18.671407', '11.04.2025'),
-    ('', 'Неверный формат даты'),
-    ('2025-04-1102:26:18.671407', 'Неверный формат даты')
-])
-def test_get_date(iso_date, expected_result):
+
+@pytest.mark.parametrize(
+    "iso_date, expected_result",
+    [
+        ("2024-03-11T02:26:18.671407", "11.03.2024"),
+        ("2025-04-11T02:26:18.671407", "11.04.2025"),
+        ("", "Неверный формат даты"),
+        ("2025-04-1102:26:18.671407", "Неверный формат даты"),
+    ],
+)
+def test_get_date(iso_date: str, expected_result: str) -> None:
+    """Функция тестирует get_date функцию со стандартными и не верными форматами данных"""
     assert get_date(iso_date) == expected_result
