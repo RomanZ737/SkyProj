aee39fb5e1aec184356f87619f78b1d942f318a7
Roman <pomanz@mail.ru>
2026-02-15 12:21:30 +0300

    decoration fixed

--- src/external_api.py
+++ src/external_api.py
@@ -1,7 +1,8 @@
-from dotenv import load_dotenv
-import requests
 import os
 from typing import Any
+
+import requests
+from dotenv import load_dotenv
 
 # Загрузка переменных из .env-файла
 load_dotenv()
