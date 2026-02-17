d6f929160d74aaac6c693b9acf0cc32c27cac23f
Roman <pomanz@mail.ru>
2026-02-17 14:01:13 +0300

    Logging for module utils added

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,13 @@
 import json
+import logging
 from typing import Any
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(filename="../logs/utils.log", mode="w")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: - %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def json_loader(file_path: str) -> Any:
@@ -8,19 +16,25 @@ def json_loader(file_path: str) -> Any:
     словарей с данными о финансовых транзакциях. Если файл пустой,
     содержит не список или не найден, функция возвращает пустой список.
     """
+    logger.info(f"Открываем файл с транзакциями {file_path}")
     try:
         with open(file_path, encoding="utf-8") as json_file:
             data = json.load(json_file)
+            logger.info(f"Файл {file_path} успешно открыт")
             return data
     except FileNotFoundError as e:
+        logger.error(f"Ошибка: {e}")
         print("Ошибка: ", e)
         return []
     except json.JSONDecodeError as e:
+        logger.error(f"Ошибка: {e}")
         print("Ошибка: ", e)
         return []
     except TypeError as e:
+        logger.error(f"Ошибка: {e}")
         print("Ошибка: ", e)
         return []
     except KeyError as e:
+        logger.error(f"Ошибка: {e}")
         print("Ошибка: ", e)
         return []
